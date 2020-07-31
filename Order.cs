5e53d453692afaaf92f1bd067691ac7c6cf10c43
ebardavid <68603486+ebardavid@users.noreply.github.com>
2020-07-31 11:31:14 -0400

    Update Order.cs

--- Order.cs
+++ Order.cs
@@ -1,8 +1,12 @@
-﻿namespace Trasnport.ly
+using System.Text.Json.Serialization;
+
+namespace Trasnport.ly
 {
     public class Order
     {
         public string Id { get; set; }
+
+        [JsonPropertyNameAttribute("destination")]
         public string Destination { get; set; }
         public Schedule Schedule { get; set; }
         public bool IsApplied() => Schedule == null ? false : true;
